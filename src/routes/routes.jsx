a13700f391674ad86b7522416d7cfc7872967eb3
bibhashhalder <halderbibhash80@gmail.com>
2024-06-03 08:25:04 +0600

    about login signup error page added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,27 @@
 import { createBrowserRouter } from "react-router-dom"
-
+import MainLayout from '../layout/MainLayout'
+import ErrorPage from "../pages/ErrorPage"
+import Home from "../pages/Home"
+import About from "../pages/About"
+import Login from "../pages/Login"
 export const router = createBrowserRouter([
-    
+    {
+        path:'/',
+        element:<MainLayout/>,
+        errorElement:<ErrorPage/>,
+        children:[
+            {
+                path:'/',
+                element:<Home/>
+            },
+            {
+                path:'/about',
+                element:<About/>
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            }
+        ]
+    }
 ])
